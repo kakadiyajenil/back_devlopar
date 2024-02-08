5187f407fd6494a3f114fead75294971033707c1
jenil kakadiya <jenilkakadiya2602@gmail.com>
2024-02-08 08:03:34 +0530

    08-02-2024

---
+++ typescript/array_touple.js
@@ -0,0 +1,16 @@
+// let a: number = 1024;
+// // b = 'HELLO';
+// console.log(a);
+// let x : number | string = 100;
+// x = 'hello'
+// console.log(x);
+// let a : Array<string> ["Hello"]
+// let a : string[] = ["Hello"]
+// a[1] = 'Skill';
+// a[20] = "100";
+// console.log(a);
+var x;
+x = ['hello', 10, 20, 'soq'];
+// x.push("qode", 20);
+// x.push(50,"qode");
+console.log(x);
